940cbcd2c2c813b9db90b5ea78b542dcc62b5874
Jason Kim <jasonkim3068@gmail.com>
2018-06-05 14:29:51 -0400

    created server with basic error handling of routes

---
+++ Bears2-Backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 8080;
+const {Server} = require('http')
+const server = Server(app)
+
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json({ limit: '50mb' }));
+app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
+
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log('There was an error connecting to the Server ', err);
+  } else {
+    console.log('You have connected to the server on PORT: ', PORT);
+  }
+});
+
+// Catches errors to routes.
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  res.status(500).send(error);
+});
+
+
+
+module.exports = app;
