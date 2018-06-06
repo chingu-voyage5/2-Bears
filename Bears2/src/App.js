37fcc9014a39191b5e447df49d49f03550ddfae6
odelavia <odelavia@gmail.com>
2018-06-06 03:33:23 -0400

    changed name of first reducer to be more syntactically meaningful

--- Bears2/src/App.js
+++ Bears2/src/App.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
-// import LibraryList from './components/LibraryList';
 
 const App = () => {
   return (
--- Bears2/src/reducers/index.js
+++ Bears2/src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
 export default combineReducers({
-  libraries: () => []
+  // change child reducers here
+  childReducer: () => []
+  //
 });
