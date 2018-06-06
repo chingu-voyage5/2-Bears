f1bebd1cbd51df190c93d8711e06f049e39e5483
odelavia <odelavia@gmail.com>
2018-06-06 03:27:26 -0400

    set up initial welcome page, intiial redux reducer and store

---
+++ Bears2/src/App.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+// import LibraryList from './components/LibraryList';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText="Welcome, User" />
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
