c7d61b30981e17e3fc1bc16c8fe2cfa20c5b573b
odelavia <odelavia@gmail.com>
2018-06-21 12:13:03 -0400

    no major changes, just reorganized some lines.

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,38 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Animated, FlatList } from 'react-native';
-import { BottomNav, ImageLoader } from './common';
+import { StyleSheet, Text, View } from 'react-native';
 import Interactable from 'react-native-interactable';
 
-const numColumns = 3
-const categories = [
-  { key: 'Category1' }, { key: 'Category2' }, { key: 'Category3' }, { key: 'Category4' }, { key: 'Category5' }, { key: 'Category6' }, { key: 'Category7' }, { key: 'Category8' }, { key: 'Category9' }, { key: 'Category10' }, { key: 'Category11' }, { key: 'Category12' }, { key: 'category13' },
-]
-const formatData = (data, numColumns) => {
-  const numberOfFullRows = Math.floor(data.length / numColumns);
-  let numberOfElementsLastRow = data.length - (numberOfFullRows * numColumns);
-  while (numberOfElementsLastRow !== numColumns && numberOfElementsLastRow !== 0) {
-    data.push({ key: `blank-${numberOfElementsLastRow}`, empty: true });
-    numberOfElementsLastRow++;
-  }
-  return data;
-};
-
 export default class CollapsingHeader extends Component {
-  constructor(props) {
-    super(props);
-    this._deltaY = new Animated.Value(0);
-  }
-  renderItem = ({ item, index }) => {
-    return (
-      <ImageLoader  item={item} />
-    );
-  };
   render() {
     return (
       <View style={styles.container}>
-        { /* <BottomNav
-          topValue={ 400 }
-        /> */ }
+        <Text>Cart Page</Text>
       </View>
     );
   }
@@ -41,5 +15,6 @@ export default class CollapsingHeader extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
   }
 });
