029c0f4e693fb29eca8fe627e0f2f010107a5e3f
Jason Kim <jasonkim3068@gmail.com>
2018-06-24 14:22:05 -0400

    updates for redux and frontend authenitcation setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,34 @@
-import React  from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { AppRegistry } from 'react-native';
+import { createLogger } from 'redux-logger';
 import reducers from './reducers';
 import { Header } from './components/common';
 import Router from './Router'
 
-const App = () => {
-  return (
-    <Provider store={createStore(reducers)}>
-      <View style={{ flex: 1 }}>
-        <Header headerText="Welcome, User" />
-        <Router />
-      </View>
-    </Provider>
-  );
-};
 
-export default App;
+const logger = createLogger({});
+const middleware = [
+    thunkMiddleware,
+    logger,
+];
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+const store = createStoreWithMiddleware(reducers);
+console.disableYellowBox = true;
+export default class App extends Component {
+
+
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={{ flex: 1 }}>
+                    <Header headerText="Welcome, User" />
+                    <Router />
+                </View>
+            </Provider>
+    );
+    }
+}
