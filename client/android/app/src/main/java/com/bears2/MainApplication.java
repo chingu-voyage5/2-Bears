ae0d812b2b7239c10602dd20b3868028fbe0548f
Oliver De La Via <odelavia@gmail.com>
2018-07-01 16:22:26 -0400

    Set up initial splash page for IOS

--- client/android/app/src/main/java/com/bears2/MainApplication.java
+++ client/android/app/src/main/java/com/bears2/MainApplication.java
@@ -3,7 +3,9 @@ package com.bears2;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.wix.interactable.Interactable;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new Interactable()
+            new SplashScreenReactPackage(),
+            new Interactable(),
+            new RNCameraPackage()
       );
     }
 
