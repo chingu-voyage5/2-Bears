05fc42c1ef5b2047ede04276ca5f944e5f81818a
Oliver De La Via <odelavia@gmail.com>
2018-07-18 11:47:06 -0400

    fixed bug in IOS file, everything is properly linked now

--- client/android/app/src/main/java/com/bears2/MainApplication.java
+++ client/android/app/src/main/java/com/bears2/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
+import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.wix.interactable.Interactable;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new SvgPackage(),
             new ImagePickerPackage(),
             new SvgPackage(),
             new SplashScreenReactPackage(),
