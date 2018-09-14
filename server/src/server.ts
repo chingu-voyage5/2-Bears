261b7c1c136c8d0a7c17bc1741152ecfb92f7be9
Simon VDB <simonvdbroeck@gmail.com>
2018-09-14 10:05:01 +0200

    refactor server to typescript

---
+++ server/src/server.ts
@@ -0,0 +1,49 @@
+import * as express from "express";
+import * as dotenv from "dotenv";
+import * as bodyparser from "body-parser";
+import { createServer } from "http";
+import * as router from "../db/api/routes/index";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.load();
+}
+
+const app = express();
+
+app.use(bodyparser.json({ limit: "50mb" }));
+app.use(bodyparser.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api", router);
+
+const server = createServer(app);
+const PORT = process.env.PORT || 5000;
+
+server.on("error", err => {
+  console.log("There was an error connecting to the Server ", err);
+});
+
+server.on("listening", () => {
+  console.log("You have connected to the server on PORT: ", PORT);
+  console.log("╔═══════════════════╗");
+  console.log("║                   ║");
+  console.log("║  Hi!              ║");
+  console.log("║    Server         ║");
+  console.log("║          is       ║");
+  console.log("║            up     ║");
+  console.log("╚═══════════════════╝");
+  console.log("Bears Backend!!");
+});
+
+server.listen(PORT);
+
+// User.findAll({
+// });
+
+// Catches errors to routes.
+app.use((err: any, _: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).send(err);
+});
+
+export default app;
