1ef8ae6d4f64610e65b816124ae33aca6e82ec01
Simon VDB <simonvdbroeck@gmail.com>
2018-09-17 22:37:15 +0200

    Fix glaring ts Errors

--- server/db/api/controller/food_items.ts
+++ server/db/api/controller/food_items.ts
@@ -1,48 +1,37 @@
 import { Router } from "express";
 import * as db from "../../models";
-import * as util from './utils';
 
 const router = Router();
 
 router.post("/", async (req, res) => {
-  console.log("this is the body", req.body);
-
   try {
-    const food_item = await db.Food_Items.findOne({ where: { food_name: req.body.food_name } });
-    if (food_item) {
-      console.log("This food category exist, please enter a food category");
-      res.status(404).send("That food category exist. Please try another food category.");
-    } else {
-      console.log("this is the body", req.body);
-      const foodItem = await db.User.create({
-        food_name: req.body.food_name,
-        food_details: req.body.food_details,
-        food_price: req.body.food_price,
-        food_category_name: req.body.food_category_name
-      });
-      // console.log('Signed Up New User: ', { user: newUser });
-      res.status(201).send({ food_item: foodItem });
-    }
+    const hasItem = await db.Food_Items.findOne({ where: { food_name: req.body.food_name } });
+
+    if (hasItem) return res.status(404).send("That food category exist. Please try another food category.");
+
+    const { food_name, food_details, food_price, food_category_name } = req.body;
+
+    const food_item = await db.Food_Items.create({
+      food_name,
+      food_details,
+      food_price,
+      food_category_name
+    });
+    return res.status(201).send({ food_item });
   } catch (error) {
-    console.log("Error creating food_category", error);
-    res.status(500).send(error);
+    return res.status(500).send(error);
   }
 });
 
 router.get("/:food_category_name", async (req, res) => {
   try {
-    console.log("this is req.params", req.params);
-    const allFoodItems = await db.Food_Items.findAll({ where: { food_category_name: req.params.food_category_name } });
-    console.log("this is the user", allFoodItems);
-    if (allFoodItems) {
-      console.log("User Logged In: ", { user: user, id_token: util.hasher(`${req.params.email}`) });
-      res.status(200).send({ food_items: allFoodItems });
-    } else {
-      res.status(404).send("there are no food items for this category");
-    }
+    const { food_category_name } = req.body;
+    const food_items = await db.Food_Items.findAll({ where: { food_category_name } });
+
+    if (!food_items) return res.status(404).send("there are no food items for this category");
+    return res.status(200).send({ food_items });
   } catch (error) {
-    console.log("Error in fetch food items", error);
-    res.status(500).send(error);
+    return res.status(500).send(error);
   }
 });
 
--- server/db/api/controller/users.ts
+++ server/db/api/controller/users.ts
@@ -1,59 +1,50 @@
 import { Router } from "express";
 import * as bcrypt from "bcrypt";
-import * as util from "./utils";
 import * as db from "../../models";
 
 const router = Router();
 const saltRounds = 10;
 
 router.post("/adduser", async (req, res) => {
-  console.log("this is the body", req.body);
-
   try {
-    console.log("hi am i even getting in here");
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(req.body.password, salt);
-    const user = await db.User.findOne({ where: { email: req.body.email } });
-    if (user) {
-      console.log("This email exist, please enter a new email address");
-      res.status(404).send("That email is taken. Please try another email.");
-    } else {
-      console.log("this is the body", req.body);
-      const newUser = await db.User.create({
-        email: req.body.email.toLowerCase(),
-        password: hash,
-        userType: 1,
-        fName: req.body.fName,
-        lName: req.body.lName,
-        username: req.body.username,
-        image: req.body.image
-      });
-      console.log("Signed Up New User: ", { user: newUser });
-      res.status(201).send({ user: newUser });
-    }
+    const { email } = req.body;
+    const hasUser = await db.User.findOne({ where: { email } });
+
+    if (hasUser) return res.status(404).send("That email is taken. Please try another email.");
+
+    const { password, fName, lName, username, image } = req.body;
+
+    const hash = await bcrypt.hash(password, saltRounds);
+
+    const user = await db.User.create({
+      email: email.toLowerCase(),
+      password: hash,
+      userType: 1,
+      fName,
+      lName,
+      username,
+      image
+    });
+
+    return res.status(201).send({ user });
   } catch (error) {
-    console.log("Error creating Users", error);
-    res.status(500).send(error);
+    return res.status(500).send(error);
   }
 });
 
 router.get("/:email/:password", async (req, res) => {
   try {
-    console.log("this is req.params", req.params);
-    const user = await db.User.findOne({ where: { email: req.params.email } });
-    console.log("this is the user", user);
-    if (user) {
-      const data = await bcrypt.compare(req.params.password, user.password);
-      if (data) {
-        console.log("User Logged In: ", { user, id_token: util.hasher(`${req.params.email}`) });
-        res.status(200).send({ user: user, id_token: util.hasher(req.params.email) });
-      } else {
-        res.status(404).send("Credentials are incorrect");
-      }
-    }
+    const { email, password } = req.params;
+
+    const user = await db.User.findOne({ where: { email } });
+    if (!user) return res.status(404).send("Credentials are incorrect");
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+
+    if(!isCorrect) return res.status(402).send('Incorrect password')
+    return res.status(200).send({ user });
   } catch (error) {
-    console.log("Error in fetch User", error);
-    res.status(500).send(error);
+    return res.status(500).send(error);
   }
 });
 
